226c4d3416f72de46f1833a52d627ae601f5a976
Jake Roper <polyamorouss@live.co.uk>
2015-07-21 22:54:55 +0100

    Basic main menu Implemented GameStateManager

---
+++ src/Main/Game.java
@@ -0,0 +1,18 @@
+package Main;
+
+import javax.swing.*;
+
+public class Game {
+
+    public static void main(String[] args){
+
+        JFrame window = new JFrame("Airasa");
+        window.setContentPane(new GamePanel());
+        window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.pack();
+        window.setVisible(true);
+
+    }
+
+}
