4a7e627ecc81c878b8b519c913f24638d716bd34
Jake Roper <polyamorouss@live.co.uk>
2015-07-26 16:15:38 +0100

    Refactored - Replaced spritesheet and level map.

--- src/Entity/Player.java
+++ src/Entity/Player.java
@@ -35,9 +35,7 @@ public class Player extends MapObject {
     //animations
     private ArrayList<BufferedImage[]> sprites;
     private final int[] numFrames = {
-
             2, 8, 1, 2, 4, 2, 5
-
     };
 
     //animation actions
